e61fc7ed555d9cecb6d7eea85813eeea9e8547ca
Nick Clawson <NickClawson@fullsail.edu>
2012-05-17 19:59:48 -0400

    created json file

---
+++ json.js
@@ -0,0 +1,60 @@
+//Author: Nicholas Clawson
+//Date: 5/15/2012
+//Assignment 3
+
+var json = {
+	"sith": [
+		{
+			"name": "Darth Teenal",
+			"captured": true,
+			"cell": 3276,
+			"lightsaber": {
+				"crystal": "red",
+				"blades": 1
+			}
+			
+		},
+		{
+			"name": "Darth Jax",
+			"captured": true,
+			"cell": 3481,
+			"lightsaber": {
+				"crystal": "red",
+				"blades": 1
+			}
+			
+		},
+		{
+			"name": "Darth Trick",
+			"captured": true,
+			"cell": 3190,
+			"lightsaber": {
+				"crystal": "red",
+				"blades": 1
+			}
+			
+		},
+		{
+			"name": "Darth Fanad",
+			"captured": false,
+			"cell": "",
+			"lightsaber": {
+				"crystal": "red",
+				"blades": 2
+			}
+			
+		},
+		{
+			"name": "Darth N'ix",
+			"captured": false,
+			"cell": "",
+			"lightsaber": {
+				"crystal": "red",
+				"blades": 2
+			}
+			
+		},
+		
+	
+	]
+};
