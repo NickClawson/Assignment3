4a4c6940bdf489157b2d2e358dab53278a0a9120
Nick Clawson <NickClawson@fullsail.edu>
2012-05-17 23:10:48 -0400

    built constructor cultMember and created 5 objects using constructor pulling information from json data

--- clawson_nicholas_assignment3.js
+++ clawson_nicholas_assignment3.js
@@ -2,8 +2,15 @@
 //Date: 5/15/2012
 //Assignment 3
 
-
-
+var cultMember = function (indx) {
+	
+	return json.sith[indx];
+};
+var teenal = cultMember(0),
+	jax = cultMember(1),
+	trick = cultMember(2),
+	fanad = cultMember(3),
+	nix = cultMember(4);
 
 
 alert("JavaScript works!");
