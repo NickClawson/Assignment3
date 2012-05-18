2511faa5d127096ecd0e8ee912c1bec034b29b56
Nick Clawson <NickClawson@fullsail.edu>
2012-05-17 22:39:55 -0400

    starting over...

--- clawson_nicholas_assignment3.js
+++ clawson_nicholas_assignment3.js
@@ -2,15 +2,6 @@
 //Date: 5/15/2012
 //Assignment 3
 
-//Fetches object from json.js and creates list
-var sithList = function (json){
-	
-return json;	
-};
-var sithCult = sithList(json);
-console.log(sithCult);
-
-
 
 
 
